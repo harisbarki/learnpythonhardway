000fdda2c5914d13fcdd12eb902101a2b3674b30
w32backdoor <harisbarki@ymail.com>
2015-01-27 11:51:46 -0500

    ex7 - done

--- ex7/ex7.py
+++ ex7/ex7.py
@@ -3,3 +3,13 @@ print "Its fleece was white as %s." % 'snow'
 print "And everywhere that Mary went."
 print "." * 10 
 
+end1 = "C"
+end2 = "H"
+end3 = "E"
+
+end7 = "B"
+end8 = "U"
+end9 = "R"
+
+print end1 + end2 + end3, #, entails that the next print stays in same line with a space though
+print end7 + end8 + end9
