1a54b76bfc9a95773b92f32156089ffe6b8e70d3
w32backdoor <harisbarki@ymail.com>
2015-01-27 14:48:26 -0500

    ex13.py

--- ex13/ex13.py
+++ ex13/ex13.py
@@ -1,8 +1,11 @@
-from sys import agrv
+from sys import argv
 
 script, first, second, third = argv
 
 print "the script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
+print "Your third variable is:", third
 
+hk = raw_input()
+print hk
