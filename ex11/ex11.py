1f1fd635aee0c4f96b2e465af5ae777eb8ba5943
w32backdoor <harisbarki@ymail.com>
2015-01-27 15:41:41 -0500

    ex11

---
+++ ex11/ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "so you are %s old and of %s height" % (age, height)
+print int(age) *int(height)
+
+print raw_input("How old are you? ")
+
