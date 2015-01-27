63e597bcb52377f883a5e4df86f7178995b1e6fa
w32backdoor <harisbarki@ymail.com>
2015-01-27 08:40:02 -0500

    organizing data

---
+++ ex5/ex5.py
@@ -0,0 +1,13 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 #not a lie
+my_height = 74 #inches
+my_eyes = "blue"
+my_hair = 'Brown'
+my_weight = 180
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+
+print "If I add %d, %d, and %d I get %d. " % ( my_age, my_height, my_weight, my_age +my_height + my_weight)
+
